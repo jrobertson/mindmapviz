10ba753cbeecbe6440571d7d9757c49b13c62dc6
Your Name <you@example.com>
2017-09-04 19:05:49 +0100

    1st commit

---
+++ lib/mindmapviz.rb
@@ -0,0 +1,113 @@
+#!/usr/bin/env ruby
+
+# file: mindmapviz.rb
+
+require 'requestor'
+
+code = Requestor.read('http://rorbuilder.info/r/ruby') do |x|
+  x.require 'pxgraphviz'
+end
+
+eval code
+
+=begin
+notes:
+
+coloured box for central point
+box for nodes
+text for leaves
+
+or
+ellipse for central point
+text for nodes and leaves 
+
+or
+
+coloure dellipse for central point 
+text for nodes and leaves
+  - larger text and thicker connections for more important things 
+coloured branches for different nodes
+
+For text only use shape: 'none'
+=end
+
+# inspired by https://github.com/bingwei/ruby-graphviz-mindmap
+
+
+class Mindmapviz
+  
+  attr_reader :raw_doc
+  
+  def initialize(s, fields: %w(label shape), delimiter: ' # ', 
+                 style: default_stylesheet())
+    
+
+
+@raw_doc=<<EOF
+<?polyrex schema='items[type, layout]/item[#{fields.join(', ')}]' delimiter='#{delimiter}'?>
+type: graph
+layout: neato
+#{s}
+EOF
+
+    @pxg = PxGraphViz.new(@raw_doc, style: style)
+ 
+  end
+  
+  def export(file='gvml.xml')
+    File.write file, @pxg.to_doc.xml(pretty: true)    
+  end
+  
+  alias export_as export
+  
+  def save(s)
+    File.write filename, @raw_doc
+  end
+  
+  def to_dot()
+    @pxg.to_dot
+  end
+
+  # writes to a PNG file (not a PNG blob)
+  #
+  def to_png(filename)    
+    @pxg.to_png filename
+  end
+  
+  # writes to a SVG file (not an SVG blob)
+  #
+  def to_svg(filename)
+    @pxg.to_svg filename
+    'SVG file written'
+  end    
+    
+  
+  private
+
+  def default_stylesheet()
+
+<<STYLE
+  node { 
+    color: #ddaa66; 
+    fillcolor: #ccffcc;
+    fontcolor: #330055; 
+    fontname: Trebuchet MS; 
+    fontsize: 8; 
+    margin: 0.0;
+    penwidth: 1; 
+    style: filled;
+  }
+
+  edge {
+    arrowsize: 0.5;
+    color: #999999; 
+    fontcolor: #444444; 
+    fontname: Verdana; 
+    fontsize: 8; 
+    #{@type == :digraph ? 'dir: forward;' : 'dir: none;'}
+    weight: 1;
+  }
+STYLE
+
+  end    
+end
---
+++ mindmapviz.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'mindmapviz'
+  s.version = '0.1.0'
+  s.summary = 'Uses Graphviz to make a mindmap.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/mindmapviz.rb']
+  s.add_runtime_dependency('pxgraphviz', '~> 0.3', '>=0.3.2')
+  s.signing_key = '../privatekeys/mindmapviz.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@jamesrobertson.eu'
+  s.homepage = 'https://github.com/jrobertson/mindmapviz'
+end
